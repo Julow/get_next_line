0d266fef16100ea4d1639e2b94fdda7e2e176219
jaguillo <juloo.dsi@gmail.com>
2014-12-30 16:36:13 +0100

    Fix reading binary

--- get_next_line.c
+++ get_next_line.c
@@ -53,7 +53,7 @@ static int		buff_read(t_gnlfd *gnlfd)
 
 	if ((tmp = (char*)malloc(S(char, gnlfd->length + GNL_BUFF))) == NULL)
 		return (-1);
-	gnl_memcpy(tmp, gnlfd->buff, gnlfd->length);
+	gnl_memcpy(tmp, gnlfd->buff, S(char, gnlfd->length));
 	if (gnlfd->buff != NULL)
 		free(gnlfd->buff - gnlfd->offset);
 	gnlfd->buff = tmp;
@@ -63,14 +63,13 @@ static int		buff_read(t_gnlfd *gnlfd)
 	return (len);
 }
 
-static int		buff_cut(t_gnlfd *gnlfd, int len, char **dst)
+static int		buff_cut(t_gnlfd *gnlfd, int len, char **dst, int rem)
 {
 	if ((*dst = (char*)malloc(S(char, len + 1))) == NULL)
 		return (GNL_ERROR);
-	gnl_memcpy(*dst, gnlfd->buff, len);
+	gnl_memcpy(*dst, gnlfd->buff, S(char, len));
 	(*dst)[len] = '\0';
-	if (gnlfd->buff[len] == '\n')
-		len++;
+	len += rem;
 	gnlfd->offset += len;
 	gnlfd->buff += len;
 	gnlfd->length -= len;
@@ -88,14 +87,14 @@ int				get_next_line(int const fd, char **line)
 			i = -1;
 			while (++i < gnlfd->length)
 				if (gnlfd->buff[i] == '\n' || gnlfd->buff[i] == EOF)
-					return (buff_cut(gnlfd, i, line));
+					return (buff_cut(gnlfd, i, line, 1));
 			if ((i = buff_read(gnlfd)) < 0)
 				break ;
 			if (i == 0 && gnlfd->length == 0)
 				return (*line = NULL, free(gnlfd->buff),
 				gnlfd->destroy = 1, GNL_EOF);
 			if (i == 0)
-				return (buff_cut(gnlfd, gnlfd->length, line));
+				return (buff_cut(gnlfd, gnlfd->length, line, 0));
 		}
 	return (GNL_ERROR);
 }
