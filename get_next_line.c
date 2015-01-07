6f4b669e8f030015460ad3fbd23139678ed22cb6
jaguillo <juloo.dsi@gmail.com>
2015-01-07 17:19:33 +0100

    Small fix

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: jaguillo <jaguillo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/12/30 11:12:36 by jaguillo          #+#    #+#             */
-/*   Updated: 2015/01/06 23:41:55 by jaguillo         ###   ########.fr       */
+/*   Updated: 2015/01/07 16:56:50 by jaguillo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,7 +41,7 @@ static t_gnlfd	*get_gnlfd(int const fd)
 	}
 	if ((tmp = (t_gnlfd*)malloc(sizeof(t_gnlfd))) == NULL)
 		return (NULL);
-	*tmp = (t_gnlfd){NULL, fd, 0, 0, -1, gnlfd_list};
+	*tmp = (t_gnlfd){NULL, fd, 0, 0, 0, gnlfd_list};
 	gnlfd_list = tmp;
 	return (tmp);
 }
@@ -61,7 +61,7 @@ static int		buff_read(t_gnlfd *gnlfd)
 	tmp[gnlfd->length + GNL_BUFF] = '\0';
 	gnlfd->length += (len < 0) ? 0 : len;
 	gnlfd->offset = 0;
-	gnlfd->i = -1;
+	gnlfd->i = 0;
 	return (len);
 }
 
@@ -73,6 +73,7 @@ static int		buff_cut(t_gnlfd *gnlfd, int len, char **dst, int rem)
 	gnlfd->offset += len;
 	gnlfd->buff += len;
 	gnlfd->length -= len;
+	gnlfd->i -= len;
 	return (GNL_SUCCES);
 }
 
@@ -84,6 +85,7 @@ int				get_next_line(int const fd, char **line)
 	if (fd >= 0 && line != NULL && (gnl = get_gnlfd(fd)) != NULL)
 		while (1)
 		{
+			gnl->i--;
 			while (++(gnl->i) < gnl->length)
 				if (gnl->buff[gnl->i] == '\n' || gnl->buff[gnl->i] == EOF)
 					return (buff_cut(gnl, gnl->i, line, 1));
@@ -91,7 +93,7 @@ int				get_next_line(int const fd, char **line)
 				break ;
 			if (len == 0 && gnl->length == 0)
 				return (*line = NULL, free(gnl->buff - gnl->offset),
-				*gnl = (t_gnlfd){NULL, fd, 0, 0, -1, gnl->next}, GNL_EOF);
+				*gnl = (t_gnlfd){NULL, fd, 0, 0, 0, gnl->next}, GNL_EOF);
 			if (len == 0)
 				return (buff_cut(gnl, gnl->length, line, 0));
 		}
